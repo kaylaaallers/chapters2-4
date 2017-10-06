227760c1cc329a9f35a3854fcd5d113e0ac2dfbf
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2017-10-06 08:36:36 -0400

    alarm cont.

--- chapter2/alarm.py
+++ chapter2/alarm.py
@@ -3,9 +3,9 @@ hourswait=float(input("How many hours are you waiting?"))
                       
 alarmtime = hourswait % 24 + timenow
 
-if alarmtime > 24:
-    alarmtime = alarmtime % 24
-    print("your alarm will go off at ", alarmtime)
+if alarmtime > 12:
+    alarmtime = alarmtime % 12
+    print("your alarm will go off at ", alarmtime, "pm")
 else:
-    print("your alarm will go off at ", alarmtime)
+    print("your alarm will go off at ", alarmtime, "am")
     #else print("your alarm will go off at ", alarmtime)
